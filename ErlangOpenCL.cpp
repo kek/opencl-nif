9ddca427fb56de380d60164b367b32875cd31ccf
Karl Eklund <localpart@gmail.com>
2020-10-23 23:57:04 +0200

    new cmake project

---
+++ ErlangOpenCL.cpp
@@ -0,0 +1,12 @@
+﻿// ErlangOpenCL.cpp : Defines the entry point for the application.
+//
+
+#include "ErlangOpenCL.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
