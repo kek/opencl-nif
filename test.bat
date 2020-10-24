2a13692161c89bef3af3f73616d0b73ef3f6a1b2
Karl Eklund <localpart@gmail.com>
2020-10-24 02:34:25 +0200

    Add test script

---
+++ test.bat
@@ -0,0 +1,2 @@
+erlc test\windows\erlang_opencl.erl
+erl -pa test\windows -noshell -s erlang_opencl bar -s init stop
--- test/windows/erlang_opencl.erl
+++ test/windows/erlang_opencl.erl
@@ -1,11 +1,16 @@
 -module(erlang_opencl).
 
--export([bar/1, foo/1]).
+-export([bar/0, bar/1, foo/1]).
 
 -on_load({init, 0}).
 
-init() -> ok = erlang:load_nif("./build/windows/Debug/erlang_opencl_nif", 0).
+init() ->
+    ok =
+        erlang:load_nif("./build/windows/Debug/erlang_opencl_nif",
+                        0).
 
 foo(_X) -> exit(nif_library_not_loaded).
 
 bar(_Y) -> exit(nif_library_not_loaded).
+
+bar() -> bar(1).
